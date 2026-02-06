13559c6cfdb07b42c06775fd7b2075356eb4fc41
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-02-06 14:08:04 +0100

    feat(pins): :sparkles: add enum class
    Co-authored-by: Xián Morales <xianmorales01@gmail.com>

---
+++ src/pins.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Pins(Enum):
+    SPAIR = ["/", lambda x, y: x - y]
+    STRIKE = ["X", 10]
+    FOUL = "0"
+    TEN = 10
+    ZERO = 0
+    ROLLS_LAST_FRAME = 3
+    ROLLS_STRIKE = 1
+    NORMAL_FRAMES_ROLLS = 2
