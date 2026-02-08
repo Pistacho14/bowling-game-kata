d87bd54aab98f020d33ed7449158d8a275be3dd1
Pablo González <pablogonzaleztemes@gmail.com>
2026-02-08 12:46:37 +0100

    refactor(pins): :recycle: rename enum type

--- src/pins.py
+++ src/pins.py
@@ -4,9 +4,9 @@ class Pins(Enum):
     SPAIR = "/"
     STRIKE = "X"
     FOUL = "0"
-    TEN = 10
+    TOTAL_PINS = 10
     ZERO = 0
     ONE = 1
-    ROLLS_LAST_FRAME = 3
+    LAST_FRAME_ROLLS = 3
     ROLLS_STRIKE = 1
     NORMAL_FRAMES_ROLLS = 2
