6c14c9cfcf00328f73ec36f916c64ac2380b863a
Pablo González <pablogonzaleztemes@gmail.com>
2026-02-08 13:24:23 +0100

    refactor(test_score_card): :recycle: remove unnecesary class method call

--- test/test_score_card.py
+++ test/test_score_card.py
@@ -66,7 +66,6 @@ def test_hitting_pins_regular():
     pins = "12345123451234512345"
     total = 60
     score_card = ScoreCard(pins)
-    score_card._split_frames()
     assert score_card.score_calculator() == total
 
 @pytest.mark.state_n
@@ -74,13 +73,11 @@ def test_symbol_zero():
     pins = "9-9-9-9-9-9-9-9-9-9-"
     total = 90
     score_card = ScoreCard(pins)
-    score_card._split_frames()
     assert score_card.score_calculator() == total
 
     pins = "9-3561368153258-7181"
     total = 82
     score_card = ScoreCard(pins)
-    score_card._split_frames()
     assert score_card.score_calculator() == total
 
 @pytest.mark.spare
@@ -88,7 +85,6 @@ def test_spare_not_extra():
     pins = "9-3/613/815/-/8-7/8-"
     total = 121
     score_card = ScoreCard(pins)
-    score_card._split_frames()
     assert score_card.score_calculator() == total
 
 @pytest.mark.strike
@@ -96,13 +92,11 @@ def test_strike():
     pins = "X9-9-9-9-9-9-9-9-9-"
     total = 100
     score_card = ScoreCard(pins)
-    score_card._split_frames()
     assert score_card.score_calculator() == total
 
     pins = "X9-X9-9-9-9-9-9-9-"
     total = 110
     score_card = ScoreCard(pins)
-    score_card._split_frames()
     assert score_card.score_calculator() == total
 
 @pytest.mark.strike
@@ -110,7 +104,6 @@ def test_two_strikes():
     pins = "XX9-9-9-9-9-9-9-9-"
     total = 120
     score_card = ScoreCard(pins)
-    score_card._split_frames()
     assert score_card.score_calculator() == total
 
 @pytest.mark.strike
@@ -118,7 +111,6 @@ def test_three_strikes():
     pins = "XXX9-9-9-9-9-9-9-"
     total = 141
     score_card = ScoreCard(pins)
-    score_card._split_frames()
     assert score_card.score_calculator() == total
 
 @pytest.mark.extra_rolls
@@ -126,13 +118,11 @@ def test_one_pin_in_extra_roll():
     pins = "9-3/613/815/-/8-7/8/8"          # Este no pasa por que en la ultima tirada hay 3 numeros.
     total = 131
     score_card = ScoreCard(pins)
-    score_card._split_frames()
     assert score_card.score_calculator() == total
 
     pins = "5/5/5/5/5/5/5/5/5/5/5"
     total = 150
     score_card = ScoreCard(pins)
-    score_card._split_frames()
     assert score_card.score_calculator() == total
 
 @pytest.mark.extra_rolls
@@ -140,7 +130,6 @@ def test_two_strikes_in_extra_rolls():
     pins = "9-9-9-9-9-9-9-9-9-XXX"
     total = 111
     score_card = ScoreCard(pins)
-    score_card._split_frames()
     assert score_card.score_calculator() == total
 
 @pytest.mark.extra_rolls
@@ -148,7 +137,6 @@ def test_one_strike_in_extra_roll():
     pins = "8/549-XX5/53639/9/X"
     total = 149
     score_card = ScoreCard(pins)
-    score_card._split_frames()
     assert score_card.score_calculator() == total
 
 @pytest.mark.extra_rolls
@@ -156,7 +144,6 @@ def test_spare_in_extra_roll():
     pins = "X5/X5/XX5/--5/X5/"
     total = 175
     score_card = ScoreCard(pins)
-    score_card._split_frames()
     assert score_card.score_calculator() == total
 
 @pytest.mark.extra_rolls
@@ -164,5 +151,4 @@ def test_triple_strike_before_extra_rolls():
     pins = "XXXXXXXXXXXX"
     total = 300
     score_card = ScoreCard(pins)
-    score_card._split_frames()
     assert score_card.score_calculator() == total
