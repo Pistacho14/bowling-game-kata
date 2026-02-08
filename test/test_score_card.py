84b1832e284d60d9ab7661446c4f3b80a4dfcdde
Pablo González <pablogonzaleztemes@gmail.com>
2026-02-08 13:06:24 +0100

    fix(test_score_card): :adhesive_bandage: fix class call

--- test/test_score_card.py
+++ test/test_score_card.py
@@ -1,10 +1,10 @@
 import pytest
-from src.score_card import Score_card
+from src.score_card import ScoreCard
 
 @pytest.mark.extra_rolls
 def test_frames_extra_roll():
     pins = "1212121212121212129/1"
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     frames = score_card.get_frames()
     assert frames == [
@@ -16,7 +16,7 @@ def test_frames_extra_roll():
 @pytest.mark.state_n
 def test_frames_rolls():
     pins = "1212121212121212129-"
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     frames = score_card.get_frames()
     assert frames == [
@@ -28,7 +28,7 @@ def test_frames_rolls():
 @pytest.mark.state_n
 def test_numerical_frames_rolls():
     pins = "1212121212121212129-"
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     frames = score_card.get_clean_frames()
     assert frames == [
@@ -40,7 +40,7 @@ def test_numerical_frames_rolls():
 @pytest.mark.state_n
 def test_numerical_frames_rolls_strike():
     pins = "X12X1212121212129-"
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     frames = score_card.get_clean_frames()
     assert frames == [
@@ -52,7 +52,7 @@ def test_numerical_frames_rolls_strike():
 @pytest.mark.state_n
 def test_numerical_frames_rolls_spare():
     pins = "1/121/1212121212129-"
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     frames = score_card.get_clean_frames()
     assert frames == [
@@ -66,7 +66,7 @@ def test_hitting_pins_regular():
     # Hitting pins total = 60
     pins = "12345123451234512345"
     total = 60
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     assert score_card.score_calculator() == total
 
@@ -75,13 +75,13 @@ def test_symbol_zero():
     # test symbol
     pins = "9-9-9-9-9-9-9-9-9-9-"
     total = 90
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     assert score_card.score_calculator() == total
 
     pins = "9-3561368153258-7181"
     total = 82
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     assert score_card.score_calculator() == total
 
@@ -90,7 +90,7 @@ def test_spare_not_extra():
     # test spare not extra
     pins = "9-3/613/815/-/8-7/8-"
     total = 121
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     assert score_card.score_calculator() == total
 
@@ -99,13 +99,13 @@ def test_strike():
     # test strike
     pins = "X9-9-9-9-9-9-9-9-9-"
     total = 100
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     assert score_card.score_calculator() == total
 
     pins = "X9-X9-9-9-9-9-9-9-"
     total = 110
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     assert score_card.score_calculator() == total
 
@@ -114,7 +114,7 @@ def test_two_strikes():
     # two strikes in a row is a double
     pins = "XX9-9-9-9-9-9-9-9-"
     total = 120
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     assert score_card.score_calculator() == total
 
@@ -123,7 +123,7 @@ def test_three_strikes():
     # three strikes in a row is a triple
     pins = "XXX9-9-9-9-9-9-9-"
     total = 141
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     assert score_card.score_calculator() == total
 
@@ -132,13 +132,13 @@ def test_one_pin_in_extra_roll():
     # one pin in extra roll
     pins = "9-3/613/815/-/8-7/8/8"          # Este no pasa por que en la ultima tirada hay 3 numeros.
     total = 131
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     assert score_card.score_calculator() == total
 
     pins = "5/5/5/5/5/5/5/5/5/5/5"
     total = 150
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     assert score_card.score_calculator() == total
 
@@ -147,7 +147,7 @@ def test_two_strikes_in_extra_rolls():
     # two strikes in extra rolls
     pins = "9-9-9-9-9-9-9-9-9-XXX"
     total = 111
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     assert score_card.score_calculator() == total
 
@@ -156,7 +156,7 @@ def test_one_strike_in_extra_roll():
     # one strike in extra roll
     pins = "8/549-XX5/53639/9/X"
     total = 149
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     assert score_card.score_calculator() == total
 
@@ -165,7 +165,7 @@ def test_spare_in_extra_roll():
     # spare in extra roll
     pins = "X5/X5/XX5/--5/X5/"
     total = 175
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     assert score_card.score_calculator() == total
 
@@ -175,6 +175,6 @@ def test_triple_strike_before_extra_rolls():
     # 2 strikes in extra rolls
     pins = "XXXXXXXXXXXX"
     total = 300
-    score_card = Score_card(pins)
+    score_card = ScoreCard(pins)
     score_card._split_frames()
     assert score_card.score_calculator() == total
